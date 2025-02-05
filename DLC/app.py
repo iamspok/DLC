ae3bfb5bbba4860ec8e597b92fe28769c2d8e940
iamspok <131056769+iamspok@users.noreply.github.com>
2025-02-05 17:59:24 +0000

    Update app.py

--- DLC/app.py
+++ DLC/app.py
@@ -23,11 +23,11 @@ try:
     # Step 3: Select random questions
     while len(selected_questions) < 15:
         question_row = df_filtered.sample(n=1).iloc[0]
-        question = question_row['A']
-        correct_answer = question_row['B']
+        question = question_row['Questions']
+        correct_answer = question_row['Correct Answer']
         answers = [correct_answer]
 
-        incorrect_answers = [question_row[col] for col in ['C', 'D', 'E', 'F'] if question_row[col] != '/']
+        incorrect_answers = [question_row[col] for col in ['Answer 2', 'Answer 3', 'Answer 4', 'Answer 5'] if question_row[col] != '/']
         answers.extend(random.sample(incorrect_answers, min(3, len(incorrect_answers))))
         random.shuffle(answers)
 
